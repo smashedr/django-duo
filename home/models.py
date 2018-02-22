92885b48a6e5c6625cb17050eed5719b5e253bdc
Shane <shane@shanerice.me>
2018-02-21 20:41:45 -0800

    Removed unused stuff

--- home/models.py
+++ home/models.py
@@ -1,14 +1,3 @@
-from __future__ import unicode_literals
-from django.utils.timezone import now
-from django.db import models
+# from __future__ import unicode_literals
 
-
-class ShareData(models.Model):
-    share_owner = models.CharField(
-        'Share Owner', max_length=50, primary_key=True
-    )
-    share_id = models.CharField(
-        'Share ID', max_length=255, default='reserved_fore_future_use'
-    )
-    generated_at = models.DateTimeField('Data Generated Date', default=now)
-    securities = models.TextField('Securities JSON', default='')
+# from django.db import models
